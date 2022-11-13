fc0e61cba3930f9277ae9c349181a3b367271115
Dylan Fox <dylan.fox.kc@gmail.com>
2022-11-12 21:57:51 -0600

    remove accordion code

--- src/components/CustomAccordion/CustomAccordion.js
+++ src/components/CustomAccordion/CustomAccordion.js
@@ -20,66 +20,6 @@ function CustomAccordion() {
           </Accordion.Body>
         </Accordion.Item>
       )}
-      {/* <Accordion.Item eventKey="0">
-        <Accordion.Header className="asc">ASC</Accordion.Header>
-        <Accordion.Body>
-          <ListGroup variant="flush">
-            {announcements["Academic Student Council"]["announcements"].map((ann, index) =>
-              <ListGroup.Item>{ann}</ListGroup.Item>
-            )}
-          </ListGroup>
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header className="chc">CHC</Accordion.Header>
-        <Accordion.Body>
-          <ListGroup variant="flush">
-            {announcements["Council of House Chaplains"]["announcements"].map((ann, index) =>
-              <ListGroup.Item>{ann}</ListGroup.Item>
-            )}
-          </ListGroup>
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="2">
-        <Accordion.Header className="cosa">COSA</Accordion.Header>
-        <Accordion.Body>
-          <ListGroup variant="flush">
-            {announcements["COSA"]["announcements"].map((ann, index) =>
-              <ListGroup.Item>{ann}</ListGroup.Item>
-            )}
-          </ListGroup>
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="3">
-        <Accordion.Header className="idea">IDEA</Accordion.Header>
-        <Accordion.Body>
-          <ListGroup variant="flush">
-            {announcements["IDEA"]["announcements"].map((ann, index) =>
-              <ListGroup.Item>{ann}</ListGroup.Item>
-            )}
-          </ListGroup>
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="4">
-        <Accordion.Header className="ims">IMs</Accordion.Header>
-        <Accordion.Body>
-          <ListGroup variant="flush">
-            {announcements["Intramurals"]["announcements"].map((ann, index) =>
-              <ListGroup.Item>{ann}</ListGroup.Item>
-            )}
-          </ListGroup>
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="5">
-        <Accordion.Header className="sen">Senate</Accordion.Header>
-        <Accordion.Body>
-          <ListGroup variant="flush">
-            {announcements["Senate"]["announcements"].map((ann, index) =>
-              <ListGroup.Item>{ann}</ListGroup.Item>
-            )}
-          </ListGroup>
-        </Accordion.Body>
-      </Accordion.Item> */}
     </Accordion>
   );
 }
